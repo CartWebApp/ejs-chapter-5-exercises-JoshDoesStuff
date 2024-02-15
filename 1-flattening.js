2e85947d61176e9b4692deab8c863b5c4f1cb8b4
Josh <JoshG4569@proton.me>
2024-02-15 15:07:12 -0800

    For all flat surfaces

--- 1-flattening.js
+++ 1-flattening.js
@@ -1,5 +1,7 @@
 let arrays = [[1, 2, 3], [4, 5], [6]];
 // Your code here.
-
-
+function flatten(arrays) {
+    return arrays[0].concat(arrays[1]).concat(arrays[2])
+}
+console.log(flatten(arrays))
 // → [1, 2, 3, 4, 5, 6]
