ae5a477bead8280d4442b2ed0ce680c70dae0f7e
Josh <JoshG4569@proton.me>
2024-02-15 15:45:39 -0800

    BURNING THE FOOD OH GOD I AM NOT COOKING

--- 3-everything.js
+++ 3-everything.js
@@ -1,10 +1,29 @@
 function every(array, test) {
-    // Your code here.
-  }
+  let arrayLength = array.length - 1
+  let i = arrayLength
+  while (arrayLength  >= 0) {
+    if (test(array[i]) === true){
+      i = i - 1
+    } else {
+      return false
+    }
+  } 
+}
+
+function everySome(array, test) {
+
+}
   
   console.log(every([1, 3, 5], n => n < 10));
   // → true
   console.log(every([2, 4, 16], n => n < 10));
   // → false
   console.log(every([], n => n < 10));
+  // → true
+
+  console.log(everySome([1, 3, 5], n => n < 10));
+  // → true
+  console.log(everySome([2, 4, 16], n => n < 10));
+  // → false
+  console.log(everySome([], n => n < 10));
   // → true
