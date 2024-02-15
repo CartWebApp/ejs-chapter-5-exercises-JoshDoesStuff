614d20048abab9daa0f6c85d1f9da69c9e167191
Josh <JoshG4569@proton.me>
2024-02-15 15:34:17 -0800

    LOOP

--- 2-your-own-loop.js
+++ 2-your-own-loop.js
@@ -1,6 +1,14 @@
 // Your code here.
+function loop(n, tester, modifier, logitbiatch) {
+    while (tester(n)) {
+        logitbiatch(n)
+        n = modifier(n)
 
-loop(3, n => n > 0, n => n - 1, console.log);
+    }
+}
+
+
+loop(271, n => n > 0, n => n - 1, console.log);
 // → 3
 // → 2
 // → 1
